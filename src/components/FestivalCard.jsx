4568a3012eb41a1c090b6cdf2bb0128698f2d488
dip04-eng <prakashmetla2020@gmail.com>
2026-01-28 01:18:46 +0530

    feat: Remove heart/wishlist button from FestivalCard and SweetCard components for cleaner UI

--- src/components/FestivalCard.jsx
+++ src/components/FestivalCard.jsx
@@ -71,15 +71,6 @@ const FestivalCard = ({ sweet, onAddToCart }) => {
             }}
           />
         </div>
-        
-        {/* Heart/Wishlist button */}
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-20">
-          <button className="w-10 h-10 bg-white rounded-full shadow-lg flex items-center justify-center hover:shadow-xl transition-shadow">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 hover:text-red-500 transition-colors" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
-          </button>
-        </div>
       </div>
 
       {/* Content */}
